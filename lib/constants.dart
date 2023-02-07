58fb8d57dcdc45205cf647fd8d5c55f8006e800e
YosrGamal <yosr.elqott@gmail.com>
2023-02-07 16:59:48 +0200

    added constant values for colors

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+//Color pallete
+const kPrimaryColor = Color.fromRGBO(216, 139, 74, 1);
+const kDarkColor = Color.fromRGBO(184, 109, 46, 1);
+const kLightColor = Color.fromRGBO(255, 220, 191, 1);
+const kTextColor = Color.fromRGBO(159, 79, 12, 1);
+const kWhiteColor = Color.fromARGB(255, 239, 237, 237);
+const kGreyColor = Color.fromARGB(255, 190, 188, 188);
