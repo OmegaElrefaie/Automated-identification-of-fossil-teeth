46f9b520551ef0c3152c89b29562e5b722e60418
YosrGamal <yosr.elqott@gmail.com>
2023-02-07 17:00:21 +0200

    get color widget

---
+++ lib/view/user/widgets/getcolor.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+MaterialStateProperty<Color> getColor(Color color, Color colorPressed) {
+  // ignore: prefer_function_declarations_over_variables
+  final getColor = (Set<MaterialState> states) {
+    if (states.contains(MaterialState.pressed)) {
+      return colorPressed;
+    } else {
+      return color;
+    }
+  };
+  return MaterialStateProperty.resolveWith(getColor);
+}
