9d5be892d50e1583d99e8ebc14491d5ed5f87915
Dinaa19 <dina_elrefaai@hotmail.com>
2023-04-11 23:58:36 +0200

    adding comments

--- lib/view/user/screens/footer_change_pass.dart
+++ lib/view/user/screens/footer_change_pass.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print, await_only_futures, prefer_interpolation_to_compose_strings
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
