93dc931a2d5fa602ddef04e2bce7be9a63377246
OmegaElrefaie <omegaelrefaie@gmail.com>
2023-04-12 02:44:49 +0200

    removing import of deleted file

--- lib/view/user/widgets/notifications.dart
+++ lib/view/user/widgets/notifications.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest.dart' as tz;
-import 'messageswidget.dart';
 
 class App_Notification {
   void notifications() async {
