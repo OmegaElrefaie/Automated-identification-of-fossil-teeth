c265403e09f38f22a0ca832eae7f20b50992285d
OmegaElrefaie <omegaelrefaie@gmail.com>
2023-04-12 02:44:27 +0200

    notification code and file

---
+++ lib/view/user/widgets/notifications.dart
@@ -0,0 +1,53 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest.dart' as tz;
+import 'messageswidget.dart';
+
+class App_Notification {
+  void notifications() async {
+    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('ic_launcher');
+
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: initializationSettingsAndroid,
+    );
+
+    flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+    tz.initializeTimeZones();
+
+    final detroit = tz.getLocation('America/Detroit');
+    var currentDateTime =
+        tz.TZDateTime.now(detroit).add(const Duration(seconds: 24));
+
+    var androidDetails = const AndroidNotificationDetails(
+      "notification 1",
+      "App Notification 1",
+      importance: Importance.max,
+      priority: Priority.high,
+      color: Colors.blue,
+      playSound: true,
+      // timeoutAfter: 5000,
+    );
+    var generalNotificationDetails =
+        NotificationDetails(android: androidDetails);
+
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      0,
+      "app title",
+      "body",
+      currentDateTime,
+      generalNotificationDetails,
+      androidAllowWhileIdle: true,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.wallClockTime,
+    );
+  }
+}
