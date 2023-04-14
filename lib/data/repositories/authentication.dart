4da3e622a3535fc247296a1d02254d7cc816da8a
YosrGamal <yosr.elqott@gmail.com>
2023-04-14 20:30:28 +0200

    authentication file

---
+++ lib/data/repositories/authentication.dart
@@ -0,0 +1,127 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
+
+class Auth {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> registerWithEmailAndPassword(
+      String email, String password) async {
+    await _auth.createUserWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+  Future<User?> registerUsingEmailPassword({
+    required BuildContext context,
+    required String email,
+    required String password,
+  }) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+    try {
+      UserCredential userCredential = await auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      user = userCredential.user;
+      user = auth.currentUser;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'email-already-in-use') {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          behavior: SnackBarBehavior.floating,
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          content: AwesomeSnackbarContent(
+              title: "Email Taken",
+              message:
+                  "An account already exists for that email. Please try an another email.",
+              contentType: ContentType.failure),
+        ));
+      } else if (e.code == 'invalid-email') {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          behavior: SnackBarBehavior.floating,
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          content: AwesomeSnackbarContent(
+              title: 'Invalid Email Format',
+              message:
+                  'It appears you have entered an invalid email format. Please try again.',
+              contentType: ContentType.failure),
+        ));
+      } else
+        print(e);
+    }
+    return user;
+  }
+
+  Future<User?> signInUsingEmailPassword({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+
+    try {
+      UserCredential userCredential = await auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      user = userCredential.user;
+      user = auth.currentUser;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          behavior: SnackBarBehavior.floating,
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          content: AwesomeSnackbarContent(
+              title: 'Wrong Email',
+              message:
+                  'It appears there are no users found for that email. Please try again.',
+              contentType: ContentType.failure),
+        ));
+      } else if (e.code == 'wrong-password') {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          behavior: SnackBarBehavior.floating,
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          content: AwesomeSnackbarContent(
+              title: 'Wrong Password',
+              message:
+                  'It appears you have entered the wrong password. Please try again.',
+              contentType: ContentType.failure),
+        ));
+      } else if (e.code == 'invalid-email') {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          behavior: SnackBarBehavior.floating,
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          content: AwesomeSnackbarContent(
+              title: 'Invalid Email Format',
+              message:
+                  'It appears you have entered an invalid email format. Please try again.',
+              contentType: ContentType.failure),
+        ));
+      } else {
+        print(e);
+      }
+    }
+    return user;
+  }
+
+  Future<void> loginInWithEmailAndPassword(
+      String email, String password) async {
+    await _auth.signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  Future<void> logout() async => await _auth.signOut();
+}
+
+
+
+
+
+
+
+
