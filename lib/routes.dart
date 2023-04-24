387d91f69880ccbc9882f6e529d21796b1b16f36
OmegaElrefaie <omegaelrefaie@gmail.com>
2023-04-24 21:04:25 +0200

    adding routes to fossils timeline and fossils map

--- lib/routes.dart
+++ lib/routes.dart
@@ -6,12 +6,14 @@ import 'package:graduation_project/view/user/screens/changepassword.dart';
 import 'package:graduation_project/view/user/screens/display_results.dart';
 import 'package:graduation_project/view/user/screens/editprofile.dart';
 import 'package:graduation_project/view/user/screens/facts.dart';
+import 'package:graduation_project/view/user/screens/fossil_map.dart';
 import 'package:graduation_project/view/user/screens/login.dart';
 import 'package:graduation_project/view/user/screens/question.dart';
 import 'package:graduation_project/view/user/screens/chat.dart';
 import 'package:graduation_project/view/user/screens/startpage.dart';
 import 'package:graduation_project/view/user/screens/signup.dart';
 import 'package:graduation_project/domain/user_model.dart';
+import 'package:graduation_project/view/user/screens/timeline_fossils.dart';
 import 'package:graduation_project/view/user/screens/userprofile.dart';
 import 'view/user/screens/library.dart';
 import 'view/user/screens/setting.dart';
@@ -43,28 +45,37 @@ final GoRouter router = GoRouter(routes: <GoRoute>[
       builder: (BuildContext context, GoRouterState state) {
         return const Fact();
       }),
+  GoRoute(
+      path: '/fossil_map',
+      builder: (BuildContext context, GoRouterState state) {
+        return const FossilMap();
+      }),
+  GoRoute(
+      path: '/timeline_fossils',
+      builder: (BuildContext context, GoRouterState state) {
+        return const TimeLine_fossils();
+      }),
   // GoRoute(
   //     path: '/question',
   //     builder: (BuildContext context, GoRouterState state) {
   //        return Question();
   //     }),
- GoRoute(
-            path: '/chat',
-            builder: (BuildContext context, GoRouterState state) {
-              return Question2(
-                currentUser: UserModel(
-                  id: 'C6rjeHZUimSKyBqUHqV0LChtbBg2',
-                  username: 'Hagar',
-                  email: 'hagar@gmail.com',
-                  //profilepic: 'assets/images/asset3.png',
-                ),
-                friendId: 'z4bQLoimq8a99z2SW6kkslUHd4g1',
-                friendName: 'Sara',
-               // friendImage: 'assets/images/asset3.png',
-              );
-            },
-          ),
-
+  GoRoute(
+    path: '/chat',
+    builder: (BuildContext context, GoRouterState state) {
+      return Question2(
+        currentUser: UserModel(
+          id: 'C6rjeHZUimSKyBqUHqV0LChtbBg2',
+          username: 'Hagar',
+          email: 'hagar@gmail.com',
+          //profilepic: 'assets/images/asset3.png',
+        ),
+        friendId: 'z4bQLoimq8a99z2SW6kkslUHd4g1',
+        friendName: 'Sara',
+        // friendImage: 'assets/images/asset3.png',
+      );
+    },
+  ),
 
   GoRoute(
       path: '/userprofile',
