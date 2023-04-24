441d8cb8901fd9b80cd4ecd38605851daa9dd57a
hagar mohamed <hagar1911783@miuegypt.edu.eg>
2023-04-24 17:39:39 +0200

    chat

---
+++ lib/view/user/screens/chat.dart
@@ -0,0 +1,112 @@
+//import 'package:go_router/go_router.dart';
+import 'package:graduation_project/constants.dart';
+import 'package:graduation_project/domain/user_model.dart';
+import 'package:graduation_project/view/user/screens/question.dart';
+import 'package:graduation_project/view/user/widgets/message_textfield.dart';
+import 'package:graduation_project/view/user/widgets/single_message.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class Question2 extends StatelessWidget {
+  final UserModel currentUser ; 
+  final String friendId;
+  final String friendName;
+  //final String friendImage;
+Question2({
+  required this.friendId,
+  required this.friendName,
+ // required this.friendImage,
+  required this.currentUser,
+});  
+  @override
+  
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:  AppBar(
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        flexibleSpace: SafeArea(
+          child: Container(
+            padding:const EdgeInsets.only(right: 16),
+            child: Row(
+              children: <Widget>[
+                IconButton(
+                  onPressed: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>Question(currentUser)));
+                    //Navigator.pop(context);
+                  },
+                  icon:const Icon(Icons.arrow_back, color: kTextColor,),
+                ),
+                const SizedBox(width: 2,),
+                const CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/asset1.png'),
+                  maxRadius: 35,
+                ),
+                const SizedBox(width: 12,),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const <Widget>[
+                      Text("Dr. Hesham Sallam",style: TextStyle( fontSize: 18 ,fontWeight: FontWeight.w600),),
+                      SizedBox(height: 6,),
+                      Text("Online",style: TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontSize: 13),),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+
+      body: Column(
+        children: [
+           Expanded(child: Container(
+             padding:const EdgeInsets.all(10),
+             decoration: const BoxDecoration(
+               color: Colors.white,
+               borderRadius: BorderRadius.only(
+                 topLeft: Radius.circular(25),
+                 topRight: Radius.circular(25)
+               )
+             ),
+             
+             child: StreamBuilder(
+              stream: FirebaseFirestore.instance
+                    .collection("Users")
+                    .doc(currentUser.id)
+                    .collection('messages')
+                    .doc(friendId)
+                    .collection('chats')
+                    .orderBy("date", descending: true)
+                    .snapshots(),
+               builder: (context,AsyncSnapshot snapshot){
+                   if(snapshot.hasData){
+                     if(snapshot.data.docs.length < 1){
+                       return const Center(
+                         child: Text("No questions yet"),
+                       );
+                     }
+                     return ListView.builder( 
+                       itemCount: snapshot.data.docs.length, 
+                       reverse: true,    
+                       physics: const BouncingScrollPhysics(),  
+                       itemBuilder: (context,index){
+                          bool isMe = snapshot.data.docs[index]['senderId'] == currentUser.id;  
+                          return SingleMessage(message: snapshot.data.docs[index]['message'], isMe: isMe);
+                       });
+                   }
+                   return const Center(
+                     child: CircularProgressIndicator()
+                   );
+               }),
+           )),
+          MessageTextField(currentUser.id ?? "", friendId),
+        ],
+      ),
+      
+    );
+  }
+}
