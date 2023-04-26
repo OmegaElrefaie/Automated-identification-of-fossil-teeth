1a4deb67ebc88628dcc1a5518f1bb28fbd69c466
OmegaElrefaie <omegaelrefaie@gmail.com>
2023-04-26 10:35:51 +0200

    Custom notification screen for admin updated

--- lib/view/user/screens/notifications_sending.dart
+++ lib/view/user/screens/notifications_sending.dart
@@ -1,59 +1,130 @@
-// import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
-// class NotificationSending extends StatefulWidget {
-//   @override
-//   _MyFormState createState() => _MyFormState();
-// }
+class CustomNotification extends StatefulWidget {
+  @override
+  _CustomNotificationState createState() => _CustomNotificationState();
+}
 
-// class _MyFormState extends State<MyForm> {
-//   final _formKey = GlobalKey<FormState>();
-//   String _inputText = '';
+class _CustomNotificationState extends State<CustomNotification> {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _bodyController = TextEditingController();
 
-//   void _submitForm() {
-//     if (_formKey.currentState!.validate()) {
-//       _formKey.currentState!.save();
-//       print('Input text: $_inputText');
-//       // do something with the input text here
-//     }
-//   }
+  final CollectionReference _customNotificationsCollection =
+      FirebaseFirestore.instance.collection('CustomNotifications');
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('My Form'),
-//       ),
-//       body: Padding(
-//         padding: EdgeInsets.all(16.0),
-//         child: Form(
-//           key: _formKey,
-//           child: Column(
-//             crossAxisAlignment: CrossAxisAlignment.start,
-//             children: <Widget>[
-//               TextFormField(
-//                 decoration: InputDecoration(
-//                   labelText: 'Input Text',
-//                   border: OutlineInputBorder(),
-//                 ),
-//                 validator: (value) {
-//                   if (value == null || value.isEmpty) {
-//                     return 'Please enter some text';
-//                   }
-//                   return null;
-//                 },
-//                 onSaved: (value) {
-//                   _inputText = value!;
-//                 },
-//               ),
-//               SizedBox(height: 16.0),
-//               ElevatedButton(
-//                 onPressed: _submitForm,
-//                 child: Text('Submit'),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+  bool _isSubmitting = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notification'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            TextField(
+              controller: _titleController,
+              decoration: InputDecoration(
+                labelText: 'Title',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: _bodyController,
+              maxLines: 5,
+              decoration: InputDecoration(
+                labelText: 'Body',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              child:
+                  _isSubmitting ? CircularProgressIndicator() : Text('Submit'),
+              onPressed: () async {
+                // Get the title and body values from the text fields
+                String title = _titleController.text.trim();
+                String body = _bodyController.text.trim();
+
+                if (title.isEmpty || body.isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Title and body cannot be empty.'),
+                    ),
+                  );
+                  return;
+                }
+
+                // Ask the user to confirm before submitting
+                bool confirmed = await showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text('Confirm'),
+                    content: Text(
+                        'Are you sure you want to submit this notification?'),
+                    actions: <Widget>[
+                      TextButton(
+                        child: Text('Cancel'),
+                        onPressed: () {
+                          Navigator.of(context).pop(false);
+                        },
+                      ),
+                      TextButton(
+                        child: Text('Submit'),
+                        onPressed: () {
+                          Navigator.of(context).pop(true);
+                        },
+                      ),
+                    ],
+                  ),
+                );
+
+                if (confirmed) {
+                  // Set the submitting state to true
+                  setState(() {
+                    _isSubmitting = true;
+                  });
+
+                  // Save the notification data to Firestore
+                  await _customNotificationsCollection.add({
+                    'Title': title,
+                    'Body': body,
+                  });
+
+                  // Show a success message to the user
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text('Notification Saved'),
+                      content: Text('Notification has been sent to all users.'),
+                      actions: <Widget>[
+                        TextButton(
+                          child: Text('OK'),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+
+                  // Reset the text fields and the submitting state
+                  _titleController.clear();
+                  _bodyController.clear();
+                  setState(() {
+                    _isSubmitting = false;
+                  });
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
